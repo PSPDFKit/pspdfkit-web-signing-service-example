9144215bc38c204eb88e0f5abd3dc8b10240c380
Claudio Ortolina <claudio@pspdfkit.com>
2019-11-07 18:42:24 +0300

    Digital Signatures - Signing service reference implementation
    Based on Express and Node Forge.
    
    Supports signing a document via an external service that receives the
    relevant content portion and hash.
    
    Features:
    
    - Uses custom certificate and private key (with instructions on how to generate/renew)
    - Include public key in signature
    - Supports minimal signing api
    - Dockerized
    - Includes some functional tests
    - Signing token handling example
    
    Resources used:
    
    - https://github.com/digitalbazaar/forge#pkcs7
    - https://www.ibm.com/support/knowledgecenter/en/SSMNED_5.0.0/com.ibm.apic.cmc.doc/task_apionprem_gernerate_self_signed_openSSL.html
    - https://github.com/vbuch/node-signpdf/blob/0b6ec1db96e2c6c06d7141f664c61d24ea3483fe/src/signpdf.js#L85-L155
    - https://tools.ietf.org/html/rfc2985#page-12

---
+++ __tests__/app.spec.js
@@ -0,0 +1,56 @@
+const request = require("supertest");
+const app = require("../lib/app");
+const forge = require("node-forge");
+const path = require("path");
+const fs = require("fs");
+
+const digest = "7B25E5DAE8FF485FBEE9D9292E1DF93686BE89336871394CE2778F0B3E4C3F91";
+const contentsFixture = path.resolve(__dirname, `fixtures/${digest}.dat`);
+const encodedContents = fs.readFileSync(contentsFixture).toString("base64");
+
+const toBeAValidSignatureResponse = response => {
+  expect(() => {
+    forge.asn1.fromDer(response.body.toString("binary"));
+  }).not.toThrow();
+};
+
+describe("App test", () => {
+  test("GET /", () => {
+    return request(app)
+      .get("/")
+      .expect(200);
+  });
+
+  describe("POST /sign", () => {
+    describe("For a user with signing rights", () => {
+      test("responds with a valid PKCS7 container", () => {
+        const payload = {
+          document_digest: digest,
+          encoded_contents: encodedContents,
+          signing_token: "user-1-with-rights"
+        };
+
+        return request(app)
+          .post("/sign")
+          .send(payload)
+          .expect(200)
+          .expect(toBeAValidSignatureResponse);
+      });
+    });
+
+    describe("For a user without signing rights", () => {
+      test("returns unauthorized", () => {
+        const payload = {
+          document_digest: digest,
+          encoded_contents: encodedContents,
+          signing_token: "user-2-with-rights"
+        };
+
+        return request(app)
+          .post("/sign")
+          .send(payload)
+          .expect(401);
+      });
+    });
+  });
+});
